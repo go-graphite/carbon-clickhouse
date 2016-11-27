8279eb886b023f2106a09b787a282fd34b8a019c
Roman Lomonosov <r.lomonosov@gmail.com>
2016-11-28 01:01:23 +0300

    rotate buffers in parser benchmark

--- receiver/plain_test.go
+++ receiver/plain_test.go
@@ -8,25 +8,37 @@ import (
 
 func BenchmarkPlainParseBuffer(b *testing.B) {
 	days := &DaysFrom1970{}
-	buf := GetBuffer()
 	out := make(chan *WriteBuffer, 1)
 
 	c1 := uint32(0)
 	c2 := uint32(0)
 
 	now := time.Now().Unix()
-	msg := fmt.Sprintf("carbon.agents.localhost.cache.size 1412351 %d\n", now)
-	buf.Time = uint32(now)
 
+	msg := fmt.Sprintf("carbon.agents.localhost.cache.size 1412351 %d\n", now)
+	buf := GetBuffer()
+	buf.Time = uint32(now)
 	for i := 0; i < 50; i++ {
 		buf.Write([]byte(msg))
 	}
 
+	msg2 := fmt.Sprintf("carbon.agents.server.udp.received 42 %d\n", now)
+	buf2 := GetBuffer()
+	buf2.Time = uint32(now)
+	for i := 0; i < 50; i++ {
+		buf2.Write([]byte(msg2))
+	}
+
 	b.ResetTimer()
 
+	var wb *WriteBuffer
 	for i := 0; i < b.N; i++ {
 		PlainParseBuffer(nil, buf, out, days, &c1, &c2)
-		wb := <-out
+		wb = <-out
+		wb.Release()
+
+		PlainParseBuffer(nil, buf2, out, days, &c1, &c2)
+		wb = <-out
 		wb.Release()
 	}
 }
