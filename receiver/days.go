387cece120e3e89a3329d9ef2f039bcdb942c7bd
Roman Lomonosov <r.lomonosov@gmail.com>
2016-11-29 15:12:58 +0300

    fix bug in days calculator

--- receiver/days.go
+++ receiver/days.go
@@ -10,18 +10,22 @@ type DaysFrom1970 struct {
 	todayDays           uint16
 }
 
-func (dd *DaysFrom1970) Timestamp(timestamp uint32) uint16 {
-	if timestamp >= dd.todayStartTimestamp && timestamp <= dd.todayStartTimestamp {
-		return dd.todayDays
-	}
-
+func (dd *DaysFrom1970) fromTimestamp(timestamp uint32) uint16 {
 	t := time.Unix(int64(timestamp), 0)
 	return uint16(time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, time.UTC).Unix() / 86400)
 }
 
+func (dd *DaysFrom1970) Timestamp(timestamp uint32) uint16 {
+	if timestamp >= dd.todayStartTimestamp && timestamp <= dd.todayEndTimestamp {
+		return dd.todayDays
+	}
+
+	return dd.fromTimestamp(timestamp)
+}
+
 func (dd *DaysFrom1970) TimestampWithNow(timestamp uint32, now uint32) uint16 {
 	if timestamp < dd.todayStartTimestamp {
-		return dd.Timestamp(timestamp)
+		return dd.fromTimestamp(timestamp)
 	}
 
 	// timestamp >= pp.todayStartTimestamp
@@ -36,7 +40,7 @@ func (dd *DaysFrom1970) TimestampWithNow(timestamp uint32, now uint32) uint16 {
 		d := time.Unix(int64(now), 0)
 		dd.todayStartTimestamp = uint32(time.Date(d.Year(), d.Month(), d.Day(), 0, 0, 0, 0, time.Local).Unix())
 		dd.todayEndTimestamp = uint32(time.Date(d.Year(), d.Month(), d.Day(), 23, 59, 59, 0, time.Local).Unix())
-		dd.todayDays = dd.Timestamp(now)
+		dd.todayDays = dd.fromTimestamp(timestamp)
 	}
 
 	return dd.Timestamp(timestamp)
