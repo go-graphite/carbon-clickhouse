eac4c33667b9fe758d4d68d7db45cc5d7637e4e6
Roman Lomonosov <r.lomonosov@gmail.com>
2016-12-08 15:30:51 +0300

    version 0.2

--- carbon-clickhouse.go
+++ carbon-clickhouse.go
@@ -18,7 +18,7 @@ import (
 )
 
 // Version of carbon-clickhouse
-const Version = "0.1"
+const Version = "0.2"
 
 func httpServe(addr string) (func(), error) {
 	tcpAddr, err := net.ResolveTCPAddr("tcp", addr)
