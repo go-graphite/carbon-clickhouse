3be9dead1f1853d505f45786079d13e1061acdf7
Roman Lomonosov <r.lomonosov@gmail.com>
2018-02-08 00:47:15 +0300

    0.7.0-alpha2

--- carbon-clickhouse.go
+++ carbon-clickhouse.go
@@ -20,7 +20,7 @@ import (
 )
 
 // Version of carbon-clickhouse
-const Version = "0.7.0-alpha"
+const Version = "0.7.0-alpha2"
 
 func httpServe(addr string) (func(), error) {
 	tcpAddr, err := net.ResolveTCPAddr("tcp", addr)
