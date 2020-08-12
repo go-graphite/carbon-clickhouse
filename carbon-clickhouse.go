0dd5cc5c9977b86a0be660f1719047f806e9dee0
Yuriy Dzyuban <yuidzyuban@avito.ru>
2020-08-12 12:19:43 +0300

    AD-12849 rolled back version change

--- carbon-clickhouse.go
+++ carbon-clickhouse.go
@@ -20,7 +20,7 @@ import (
 )
 
 // Version of carbon-clickhouse
-const Version = "0.11.1"
+const Version = "0.11.0"
 
 func httpServe(addr string) (func(), error) {
 	tcpAddr, err := net.ResolveTCPAddr("tcp", addr)
