31e1792e6423dc8c5f84fe0bcae49839c1c47eac
Yuriy Dzyuban <yuidzyuban@avito.ru>
2020-08-14 12:21:52 +0300

    AD-12849 don't change Version

--- carbon-clickhouse.go
+++ carbon-clickhouse.go
@@ -20,7 +20,7 @@ import (
 )
 
 // Version of carbon-clickhouse
-const Version = "0.11.0.custom.2"
+const Version = "0.11.0"
 
 func httpServe(addr string) (func(), error) {
 	tcpAddr, err := net.ResolveTCPAddr("tcp", addr)
