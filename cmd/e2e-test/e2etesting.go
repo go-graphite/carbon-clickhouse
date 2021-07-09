822c58a019294e520b154ee068b2aa3094de499a
msaf1980 <msaf1980@gmail.com>
2021-07-09 11:11:22 +0500

    e2e test: some refactoring

--- cmd/e2e-test/e2etesting.go
+++ cmd/e2e-test/e2etesting.go
@@ -5,8 +5,12 @@ import (
 	"io/ioutil"
 	"net"
 	"net/http"
+	"path"
 	"strings"
 	"time"
+
+	"github.com/BurntSushi/toml"
+	"go.uber.org/zap"
 )
 
 type Verify struct {
@@ -89,4 +93,196 @@ func verifyOut(address string, verify Verify) []string {
 		}
 	}
 	return errs
+}
+
+func testCarbonClickhouse(
+	test *TestSchema, clickhouse Clickhouse,
+	testDir, rootDir string,
+	verbose bool, logger *zap.Logger) (testSuccess bool) {
+
+	testSuccess = true
+
+	clickhouseDir := clickhouse.Dir // for logging
+	if !strings.HasPrefix(clickhouse.Dir, "/") {
+		clickhouse.Dir = rootDir + "/" + clickhouse.Dir
+	}
+	err, out := clickhouse.Start()
+	if err != nil {
+		logger.Error("starting clickhouse",
+			zap.String("config", test.name),
+			zap.Any("clickhouse version", clickhouse.Version),
+			zap.String("clickhouse config", clickhouseDir),
+			zap.Error(err),
+			zap.String("out", out),
+		)
+		testSuccess = false
+		clickhouse.Stop(true)
+		return
+	}
+
+	cch := CarbonClickhouse{
+		ConfigTpl: testDir + "/" + test.ConfigTpl,
+	}
+	err = cch.Start(clickhouse.Address())
+	if err != nil {
+		logger.Error("starting carbon-clickhouse",
+			zap.String("config", test.name),
+			zap.String("clickhouse version", clickhouse.Version),
+			zap.String("clickhouse config", clickhouseDir),
+			zap.Error(err),
+			zap.String("out", out),
+		)
+		testSuccess = false
+	}
+
+	if testSuccess {
+		logger.Info("starting e2e test",
+			zap.String("config", test.name),
+			zap.String("clickhouse version", clickhouse.Version),
+			zap.String("clickhouse config", clickhouseDir),
+		)
+		time.Sleep(2 * time.Second)
+		// Run test
+
+		if len(test.Input) > 0 {
+			if err = sendPlain("tcp", cch.address, test.Input); err != nil {
+				logger.Error("send plain to carbon-clickhouse",
+					zap.String("config", test.name),
+					zap.String("clickhouse version", clickhouse.Version),
+					zap.String("clickhouse config", clickhouseDir),
+					zap.Error(err),
+					zap.String("out", out),
+				)
+				testSuccess = false
+			}
+		}
+
+		if testSuccess {
+			verifyFailed := 0
+			time.Sleep(10 * time.Second)
+			for _, verify := range test.Verify {
+				if errs := verifyOut(clickhouse.Address(), verify); len(errs) > 0 {
+					testSuccess = false
+					verifyFailed++
+					for _, e := range errs {
+						logger.Error(e)
+					}
+					logger.Error("verify records in clickhouse",
+						zap.String("config", test.name),
+						zap.String("clickhouse version", clickhouse.Version),
+						zap.String("clickhouse config", clickhouseDir),
+						zap.String("verify", verify.Query),
+					)
+				} else if verbose {
+					logger.Info("verify records in clickhouse",
+						zap.String("config", test.name),
+						zap.String("clickhouse version", clickhouse.Version),
+						zap.String("clickhouse config", clickhouseDir),
+						zap.String("verify", verify.Query),
+					)
+				}
+			}
+			if verifyFailed > 0 {
+				logger.Error("verify records in clickhouse",
+					zap.String("config", test.name),
+					zap.String("clickhouse version", clickhouse.Version),
+					zap.String("clickhouse config", clickhouseDir),
+					zap.Int("verify failed", verifyFailed),
+					zap.Int("verify total", len(test.Verify)),
+				)
+			} else {
+				logger.Info("verify records in clickhouse",
+					zap.String("config", test.name),
+					zap.String("clickhouse version", clickhouse.Version),
+					zap.String("clickhouse config", clickhouseDir),
+					zap.Int("verify success", len(test.Verify)),
+					zap.Int("verify total", len(test.Verify)),
+				)
+			}
+		}
+	}
+
+	err = cch.Stop()
+	cch.Cleanup()
+	if err != nil {
+		logger.Error("stoping carbon-clickhouse",
+			zap.String("config", test.name),
+			zap.String("clickhouse version", clickhouse.Version),
+			zap.String("clickhouse config", clickhouseDir),
+			zap.Error(err),
+			zap.String("out", out),
+		)
+		testSuccess = false
+	}
+
+	err, out = clickhouse.Stop(true)
+	if err != nil {
+		logger.Error("stoping clickhouse",
+			zap.String("config", test.name),
+			zap.String("clickhouse version", clickhouse.Version),
+			zap.String("clickhouse config", clickhouseDir),
+			zap.Error(err),
+			zap.String("out", out),
+		)
+		testSuccess = false
+	}
+
+	if testSuccess {
+		logger.Info("end e2e test",
+			zap.String("config", test.name),
+			zap.String("status", "success"),
+			zap.String("clickhouse version", clickhouse.Version),
+			zap.String("clickhouse config", clickhouseDir),
+		)
+	} else {
+		logger.Error("end e2e test",
+			zap.String("config", test.name),
+			zap.String("status", "failed"),
+			zap.String("clickhouse version", clickhouse.Version),
+			zap.String("clickhouse config", clickhouseDir),
+		)
+	}
+
+	return
+}
+
+func runTest(config string, rootDir string, verbose bool, logger *zap.Logger) (failed, total int) {
+	testDir := path.Dir(config)
+	d, err := ioutil.ReadFile(config)
+	if err != nil {
+		logger.Error("failed to read config",
+			zap.String("config", config),
+			zap.Error(err),
+		)
+		failed++
+		total++
+		return
+	}
+
+	confShort := strings.ReplaceAll(config, rootDir+"/", "")
+
+	var cfg = MainConfig{}
+	if _, err := toml.Decode(string(d), &cfg); err != nil {
+		logger.Fatal("failed to decode config",
+			zap.String("config", confShort),
+			zap.Error(err),
+		)
+	}
+
+	cfg.Test.name = confShort
+
+	if len(cfg.Test.Input) == 0 {
+		logger.Fatal("input not set",
+			zap.String("config", confShort),
+		)
+	}
+
+	for _, clickhouse := range cfg.Test.Clickhouse {
+		total++
+		if !testCarbonClickhouse(cfg.Test, clickhouse, testDir, rootDir, verbose, logger) {
+			failed++
+		}
+	}
+
+	return
 }
--- cmd/e2e-test/main.go
+++ cmd/e2e-test/main.go
@@ -7,10 +7,7 @@ import (
 	"os"
 	"path"
 	"runtime"
-	"strings"
-	"time"
 
-	"github.com/BurntSushi/toml"
 	"go.uber.org/zap"
 )
 
@@ -20,6 +17,8 @@ type TestSchema struct {
 	Clickhouse []Clickhouse `yaml:"clickhouse"`
 
 	Verify []Verify `yaml:"verify"`
+
+	name string `yaml:"-"` // test alias (from config name)
 }
 
 type MainConfig struct {
@@ -102,175 +101,14 @@ func main() {
 	failed := 0
 	total := 0
 	for _, config := range configs {
-		d, err := ioutil.ReadFile(config)
-		if err != nil {
-			logger.Error("failed to read config",
-				zap.String("config", config),
-				zap.Error(err),
-			)
-			failed++
-			total++
-			continue
-		}
-
-		testDir := path.Dir(config)
-		confShort := strings.ReplaceAll(config, rootDir+"/", "")
-
-		var cfg = MainConfig{}
-		if _, err := toml.Decode(string(d), &cfg); err != nil {
-			logger.Fatal("failed to decode config",
-				zap.String("config", confShort),
-				zap.Error(err),
-			)
-		}
-
-		if len(cfg.Test.Input) == 0 {
-			logger.Fatal("input not set",
-				zap.String("config", confShort),
-			)
-		}
-
-		for _, clickhouse := range cfg.Test.Clickhouse {
-			total++
-			testFail := false
-			clickhouseDir := clickhouse.Dir // for logging
-			clickhouse.Dir = rootDir + "/" + clickhouse.Dir
-			err, out := clickhouse.Start()
-			if err != nil {
-				logger.Error("starting clickhouse",
-					zap.String("config", confShort),
-					zap.Any("clickhouse version", clickhouse.Version),
-					zap.String("clickhouse config", clickhouseDir),
-					zap.Error(err),
-					zap.String("out", out),
-				)
-				failed++
-				clickhouse.Stop(true)
-				continue
-			}
-
-			cch := CarbonClickhouse{
-				ConfigTpl: testDir + "/" + cfg.Test.ConfigTpl,
-			}
-			err = cch.Start(clickhouse.Address())
-			if err != nil {
-				logger.Error("starting carbon-clickhouse",
-					zap.String("config", confShort),
-					zap.String("clickhouse version", clickhouse.Version),
-					zap.String("clickhouse config", clickhouseDir),
-					zap.Error(err),
-					zap.String("out", out),
-				)
-				testFail = true
-			}
-
-			if !testFail {
-				logger.Info("starting e2e test",
-					zap.String("config", confShort),
-					zap.String("clickhouse version", clickhouse.Version),
-					zap.String("clickhouse config", clickhouseDir),
-				)
-				time.Sleep(2 * time.Second)
-				// Run test
-
-				if len(cfg.Test.Input) > 0 {
-					if err = sendPlain("tcp", cch.address, cfg.Test.Input); err != nil {
-						logger.Error("send plain to carbon-clickhouse",
-							zap.String("config", confShort),
-							zap.String("clickhouse version", clickhouse.Version),
-							zap.String("clickhouse config", clickhouseDir),
-							zap.Error(err),
-							zap.String("out", out),
-						)
-						testFail = true
-					}
-				}
-
-				if !testFail {
-					verifyFailed := 0
-					time.Sleep(10 * time.Second)
-					for _, verify := range cfg.Test.Verify {
-						if errs := verifyOut(clickhouse.Address(), verify); len(errs) > 0 {
-							testFail = true
-							verifyFailed++
-							for _, e := range errs {
-								logger.Error(e)
-							}
-							logger.Error("verify records in clickhouse",
-								zap.String("config", confShort),
-								zap.String("clickhouse version", clickhouse.Version),
-								zap.String("clickhouse config", clickhouseDir),
-								zap.String("verify", verify.Query),
-							)
-						} else if *verbose {
-							logger.Info("verify records in clickhouse",
-								zap.String("config", confShort),
-								zap.String("clickhouse version", clickhouse.Version),
-								zap.String("clickhouse config", clickhouseDir),
-								zap.String("verify", verify.Query),
-							)
-						}
-					}
-					if verifyFailed > 0 {
-						logger.Error("verify records in clickhouse",
-							zap.String("config", confShort),
-							zap.String("clickhouse version", clickhouse.Version),
-							zap.String("clickhouse config", clickhouseDir),
-							zap.Int("verify failed", verifyFailed),
-							zap.Int("verify total", len(cfg.Test.Verify)),
-						)
-					} else {
-						logger.Info("verify records in clickhouse",
-							zap.String("config", confShort),
-							zap.String("clickhouse version", clickhouse.Version),
-							zap.String("clickhouse config", clickhouseDir),
-							zap.Int("verify success", len(cfg.Test.Verify)),
-							zap.Int("verify total", len(cfg.Test.Verify)),
-						)
-					}
-				}
-			}
-
-			err = cch.Stop()
-			cch.Cleanup()
-			if err != nil {
-				logger.Error("stoping carbon-clickhouse",
-					zap.String("config", confShort),
-					zap.String("clickhouse version", clickhouse.Version),
-					zap.String("clickhouse config", clickhouseDir),
-					zap.Error(err),
-					zap.String("out", out),
-				)
-				testFail = true
-			}
-
-			err, out = clickhouse.Stop(true)
-			if err != nil {
-				logger.Error("stoping clickhouse",
-					zap.String("config", confShort),
-					zap.String("clickhouse version", clickhouse.Version),
-					zap.String("clickhouse config", clickhouseDir),
-					zap.Error(err),
-					zap.String("out", out),
-				)
-				testFail = true
-			}
-
-			if testFail {
-				failed++
-			} else {
-				logger.Info("end e2e test",
-					zap.String("config", confShort),
-					zap.String("clickhouse version", clickhouse.Version),
-					zap.String("clickhouse config", clickhouseDir),
-				)
-			}
-		}
+		testFailed, testTotal := runTest(config, rootDir, *verbose, logger)
+		failed += testFailed
+		total += testTotal
 	}
 
 	if failed > 0 {
 		logger.Error("tests ended",
-			zap.Bool("success", false),
+			zap.String("status", "failed"),
 			zap.Int("count", total),
 			zap.Int("failed", failed),
 			zap.Int("configs", len(configs)),
@@ -278,7 +116,7 @@ func main() {
 		os.Exit(1)
 	} else {
 		logger.Info("tests ended",
-			zap.Bool("success", true),
+			zap.String("status", "success"),
 			zap.Int("count", total),
 			zap.Int("configs", len(configs)),
 		)
