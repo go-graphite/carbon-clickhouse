de17b49839baf73cc20ced7e098b869cb6978063
msaf1980 <msaf1980@gmail.com>
2021-08-27 22:19:24 +0500

    uploader: refactor tags parse (better perfomance)

--- uploader/tagged.go
+++ uploader/tagged.go
@@ -10,6 +10,7 @@ import (
 	"time"
 
 	"github.com/lomik/carbon-clickhouse/helper/RowBinary"
+	"github.com/msaf1980/go-stringutils"
 	"go.uber.org/zap"
 )
 
@@ -52,11 +53,39 @@ func urlParse(rawurl string) (*url.URL, error) {
 	return m, err
 }
 
+// don't unescape special symbols
+// escape also not needed (all is done in receiver/plain.go, Base.PlainParseLine)
+func tagsParse(path string) (string, map[string]string, error) {
+	name, args, n := stringutils.Split2(path, "?")
+	if n == 1 || args == "" {
+		return name, nil, fmt.Errorf("incomplete tags in '%s'", path)
+	}
+	tags := make(map[string]string)
+	for {
+		if delim := strings.Index(args, "="); delim == -1 {
+			// corrupted tag
+			break
+		} else {
+			key := args[0:delim]
+			v := args[delim+1:]
+			if end := strings.Index(v, "&"); end == -1 {
+				tags[key] = args[0:]
+				break
+			} else {
+				end += delim + 1
+				tags[key] = args[0:end]
+				args = args[end+1:]
+			}
+		}
+	}
+	return name, tags, nil
+}
+
 func (u *Tagged) parseName(name string, days uint16, version uint32,
 	// reusable buffers
 	tag1 []string, wb *RowBinary.WriteBuffer, tagsBuf *RowBinary.WriteBuffer) error {
 
-	m, err := urlParse(name)
+	mPath, tags, err := tagsParse(name)
 	if err != nil {
 		return err
 	}
@@ -65,26 +94,24 @@ func (u *Tagged) parseName(name string, days uint16, version uint32,
 	tagsBuf.Reset()
 	tag1 = tag1[:0]
 
-	t := fmt.Sprintf("__name__=%s", m.Path)
+	t := "__name__=" + mPath
 	tag1 = append(tag1, t)
 	tagsBuf.WriteString(t)
+	tagsWritten := 1
 
 	// calc size for prevent buffer overflow
 	sizeTags := RowBinary.SIZE_INT16 /* days */ +
-		RowBinary.SIZE_INT64 + len(m.Path) +
+		RowBinary.SIZE_INT64 + len(mPath) +
 		RowBinary.SIZE_INT64 + len(name) +
 		RowBinary.SIZE_INT64 + //  tagsBuf.Len() not known at this step
 		RowBinary.SIZE_INT16 //version
 
 	// don't upload any other tag but __name__
 	// if either main metric (m.Path) or each metric (*) is ignored
-	ignoreAllButName := u.ignoredMetrics[m.Path] || u.ignoredMetrics["*"]
-	tagsWritten := 1
-	for k, v := range m.Query() {
-		t := fmt.Sprintf("%s=%s", k, v[0])
-
+	ignoreAllButName := u.ignoredMetrics[mPath] || u.ignoredMetrics["*"]
+	for _, tag := range tags {
 		sizeTags += RowBinary.SIZE_INT16 /* days */ +
-			RowBinary.SIZE_INT64 + len(t) +
+			RowBinary.SIZE_INT64 + len(tag) +
 			RowBinary.SIZE_INT64 + len(name) +
 			RowBinary.SIZE_INT64 + // tagsBuf.Len() not known at this step
 			RowBinary.SIZE_INT16 //version
@@ -93,11 +120,11 @@ func (u *Tagged) parseName(name string, days uint16, version uint32,
 			return errBufOverflow
 		}
 
-		tagsBuf.WriteString(t)
+		tagsBuf.WriteString(tag)
 		tagsWritten++
 
 		if !ignoreAllButName {
-			tag1 = append(tag1, t)
+			tag1 = append(tag1, tag)
 		}
 	}
 
@@ -136,7 +163,7 @@ func (u *Tagged) parseFile(filename string, out io.Writer) (uint64, map[string]b
 	defer wb.Release()
 	defer tagsBuf.Release()
 
-	tag1 := make([]string, 0)
+	tag1 := make([]string, 0, 32)
 
 LineLoop:
 	for {
