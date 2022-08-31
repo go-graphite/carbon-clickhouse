ac037f122e41cba0f47a0cfd8727581c0a114577
msaf1980 <msaf1980@gmail.com>
2022-08-31 12:14:49 +0500

    uploader: prevent buffer overlow in index parseFile

--- uploader/index_test.go
+++ uploader/index_test.go
@@ -15,6 +15,7 @@ import (
 	"github.com/lomik/carbon-clickhouse/helper/config"
 	"github.com/lomik/carbon-clickhouse/helper/tests"
 	"github.com/lomik/zapwriter"
+	"github.com/stretchr/testify/assert"
 )
 
 type indexRecord struct {
@@ -573,4 +574,38 @@ func BenchmarkIndexParseFileCompressedNoDaily(b *testing.B) {
 	}
 
 	benchmarkIndexParseFile(b, &bm, points, wantPoints)
+}
+
+func TestIndexParseName_Overflow(t *testing.T) {
+	wb := RowBinary.GetWriteBuffer()
+	defer wb.Release()
+
+	logger := zapwriter.Logger("upload")
+	base := &Base{
+		queue:   make(chan string, 1024),
+		inQueue: make(map[string]bool),
+		logger:  logger,
+		config:  &Config{TableName: "test"},
+	}
+	var sb bytes.Buffer
+	for i := 0; i < 400; i++ {
+		if i > 0 {
+			sb.WriteString(".")
+		}
+		sb.WriteString(fmt.Sprintf("very_long%d", i))
+	}
+	u := NewIndex(base)
+
+	name := sb.Bytes()
+
+	reverseName := make([]byte, len(name))
+	RowBinary.ReverseBytesTo(reverseName, name)
+
+	treeDate := uint16(DefaultTreeDate)
+	days := uint16(10)
+	version := uint32(time.Now().Unix())
+	newUniq := make(map[string]bool)
+
+	err := u.parseName(name, reverseName, treeDate, days, version, u.config.DisableDailyIndex, newUniq, wb)
+	assert.Equal(t, errBufOverflow, err)
 }
