0cbf5f160891aeee843c048995050bfb7142b4df
Michail Safronov <msaf1980@gmail.com>
2022-11-16 23:18:24 +0500

    update version to 0.11.3 (#117)

--- carbon-clickhouse.go
+++ carbon-clickhouse.go
@@ -22,7 +22,7 @@ import (
 )
 
 // Version of carbon-clickhouse
-const Version = "0.11.2"
+const Version = "0.11.3"
 
 func httpServe(addr string) (func(), error) {
 	tcpAddr, err := net.ResolveTCPAddr("tcp", addr)
