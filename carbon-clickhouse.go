aebdd8c7edeb0f653d5683964323cbf519450581
Michail Safronov <msaf1980@gmail.com>
2022-11-17 18:16:33 +0500

    Bump version to 0.11.4

--- carbon-clickhouse.go
+++ carbon-clickhouse.go
@@ -22,7 +22,7 @@ import (
 )
 
 // Version of carbon-clickhouse
-const Version = "0.11.3"
+const Version = "0.11.4"
 
 func httpServe(addr string) (func(), error) {
 	tcpAddr, err := net.ResolveTCPAddr("tcp", addr)
